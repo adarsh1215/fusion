4b3c3d128415a80b217a2b48ac845a853fa63410
alpha1215 <adarshp390@gmail.com>
2025-04-04 16:21:37 +0530

    initial commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className = 'flex flex-col justify-center items-center text-cnter my-20'>
+        <div className = 'text-stone-50 inline-flex text-cnter gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
+            <p>Best text to image generater</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
